03446ac7835c7f9d8d7080d731cd72461aad4672
ekrakov <144193466+ekrakov@users.noreply.github.com>
2024-05-08 17:19:44 -0700

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -7,8 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'BandAid'
-copyright = 'workshop participant'
-author = 'workshop participant'
+copyright = 'BandAid'
+author = 'Eliana Krakovsky'
 release = '0.1'
 
 
